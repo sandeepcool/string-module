db3f1da573132fb912ae94f869a3f5bcb6c7624d
Thatikonda Venkata Sandeep <>
2021-05-27 07:02:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+name1 = "joe"
+name2 = "jhon"
+name3 = "jack"
+print(name1,name2,name3)
+
+print(name2)
+
+print(name3)
+
+list1 = ("joe", "juli", "james")
+print(list1)
+
+list2 = (4,"jhon",3.5)
+
+print(list1[1])
+
+print(list2[2])
+
+print("joe", "juli", "james", end=" ")
+print("jack")
+
+print("My name is ", end="")
+print("Monty Python.")
+
+print("my", "name", "is", "monty", "python.", sep="*")
+print()
+
+print("Programming", "Essentials", end="***", sep="***")
+print("in", "Python", sep="...")
+print()
+
+print("        $")
+print("       $$")
+print("      $$$$")
+print("     $$$$$$")
+print("   $$      $$")
+print("  $$$      $$$")
+print(" $$$$      $$$$")
+print("$$$$$      $$$$$")
+print("   $$      $$")
+print("   $$      $$")
+print("   $$      $$")
+print("   $$$$$$$$$$")
+print("   $$$$$$$$$$")
+print()
+
